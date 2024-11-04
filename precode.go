89016f9d3b50765bc92b7bfafa2d1664b3a5032a
Evgeniy Fedotov <hardpair@yandex.ru>
2024-11-04 21:05:28 +0300

    testing using testify. Stage 1

--- precode.go
+++ precode.go
@@ -6,6 +6,8 @@ import (
     "strconv"
     "strings"
     "testing"
+
+    "github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -48,11 +50,39 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
     totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
+    req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
 
     responseRecorder := httptest.NewRecorder()
     handler := http.HandlerFunc(mainHandle)
     handler.ServeHTTP(responseRecorder, req)
 
     // здесь нужно добавить необходимые проверки
+    // добавим вспомогательных переменных
+    body := responseRecorder.Body.String()
+    bodyList := strings.Split(body, ",")
+    status := responseRecorder.Code
+
+    require.Equalf(t, http.StatusOK, status, "expected status code: %d, got %d", http.StatusOK, status)
+    require.NotEmptyf(t, body, "expected not empty responce body")
+    require.Lenf(t, bodyList, totalCount,
+        "expected caffe count: %d, got %d", totalCount, len(bodyList))
+
+}
+
+// Проверяем ответ на не поддерживаемый город (отсутствующий в базе)
+func TestMainHandlerWhenUnknownCountry(t *testing.T) {
+    wrongCity := "wrong city value"
+    req := httptest.NewRequest("GET", "/cafe?count=1&city=tula", nil)
+
+    responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
+
+    // добавим вспомогательных переменных
+    body := responseRecorder.Body.String()
+    status := responseRecorder.Code
+
+    require.Equalf(t, http.StatusBadRequest, status, "expected status code: %d, got %d", http.StatusBadRequest, status)
+    // require.NotEmptyf(t, body, "expected not empty responce body")
+    require.Equalf(t, wrongCity, body, "expected server's message: '%s', got '%s'", wrongCity, body)
 }
